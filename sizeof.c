bb056e240b04ac49cea01a13567077343da0095a
Alex Zorg <azorg@mail.ru>
2015-02-07 19:31:11 +0300

    add sizeof(long double)

--- sizeof.c
+++ sizeof.c
@@ -8,20 +8,21 @@
 int main()
 {
 #ifndef __cplusplus
-  printf("sizeof(void)      = %i\n", (int) sizeof(void));
+  printf("sizeof(void)        = %i\n", (int) sizeof(void));
 #endif // __cplusplus
 
-  printf("sizeof(char)      = %i\n", (int) sizeof(char));
-  printf("sizeof(short)     = %i\n", (int) sizeof(short));
-  printf("sizeof(int)       = %i\n", (int) sizeof(int));
-  printf("sizeof(long)      = %i\n", (int) sizeof(long));
-  printf("sizeof(long long) = %i\n", (int) sizeof(long long));
-  printf("sizeof(int32_t)   = %i\n", (int) sizeof(int32_t));
-  printf("sizeof(int64_t)   = %i\n", (int) sizeof(int64_t));
-  printf("sizeof(float)     = %i\n", (int) sizeof(float));
-  printf("sizeof(double)    = %i\n", (int) sizeof(double));
-  printf("sizeof(int*)      = %i\n", (int) sizeof(int*));
-  printf("sizeof(void*)     = %i\n", (int) sizeof(void*));
+  printf("sizeof(char)        = %i\n", (int) sizeof(char));
+  printf("sizeof(short)       = %i\n", (int) sizeof(short));
+  printf("sizeof(int)         = %i\n", (int) sizeof(int));
+  printf("sizeof(long)        = %i\n", (int) sizeof(long));
+  printf("sizeof(long long)   = %i\n", (int) sizeof(long long));
+  printf("sizeof(int32_t)     = %i\n", (int) sizeof(int32_t));
+  printf("sizeof(int64_t)     = %i\n", (int) sizeof(int64_t));
+  printf("sizeof(float)       = %i\n", (int) sizeof(float));
+  printf("sizeof(double)      = %i\n", (int) sizeof(double));
+  printf("sizeof(long double) = %i\n", (int) sizeof(long double));
+  printf("sizeof(int*)        = %i\n", (int) sizeof(int*));
+  printf("sizeof(void*)       = %i\n", (int) sizeof(void*));
 
   return 0;
 }
